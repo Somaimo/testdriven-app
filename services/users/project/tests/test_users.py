d74526191d8927d2f2d7fe270a6ae6da36ced837
Somaimo <camao@bluewin.ch>
2018-11-16 15:36:31 -0800

    added first html tempalte to user service.

--- services/users/project/tests/test_users.py
+++ services/users/project/tests/test_users.py
@@ -125,6 +125,27 @@ class TestUserService(BaseTestCase):
             self.assertIn('User does not exist', data['message'])
             self.assertIn('fail', data['status'])
 
+    def test_main_no_users(self):
+        """Ensure the main route behaves correctly when no users have been
+         added to the database."""
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'All Users', response.data)
+        self.assertIn(b'<p>No users!</p>', response.data)
+
+    def test_main_with_users(self):
+        """Ensure the main route behaves correctly when users have been
+        added to the database."""
+        add_user('marc', 'mwinkler@bluewin.ch')
+        add_user('nicole', 'nicole@bluewin.ch')
+        with self.client:
+            response = self.client.get('/')
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'All Users', response.data)
+            self.assertNotIn(b'<p>No users!</p>', response.data)
+            self.assertIn(b'marc', response.data)
+            self.assertIn(b'nicole', response.data)
+
     def test_all_users(self):
         """Ensure get all users behaves correctly."""
         add_user('marc', 'mwinkler@bluewin.ch')
@@ -141,6 +162,19 @@ class TestUserService(BaseTestCase):
             self.assertIn(
                 'nicole@bluewin.ch', data['data']['users'][1]['email'])
             self.assertIn('success', data['status'])
+
+    def test_main_add_user(self):
+        """Ensure a new user can be added through the form to the database."""
+        with self.client:
+            response = self.client.post(
+                '/',
+                data=dict(username='marc', email='mwinkler@bluewin.ch'),
+                follow_redirects=True
+            )
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'All Users', response.data)
+            self.assertNotIn(b'<p>No users!</p>', response.data)
+            self.assertIn(b'marc', response.data)
 
 
 if __name__ == '__main__':
